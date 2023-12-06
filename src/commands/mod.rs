d5107f0770a86f6366846bc8ab329f18f4c2b679
Michael P. Jung <michael.jung@terreon.de>
2023-12-06 12:20:51 +0100

    Move all cli logic into cli module

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,50 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+use crate::config::Config;
+
 pub mod clean;
 pub mod config;
 pub mod query;
 pub mod schema;
+
+#[derive(Debug, Parser)]
+#[command(name = "tusker")]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    Schema(schema::SchemaCommand),
+    /// Remove all temporary databases, schemas and tables created by
+    /// tusker
+    Clean(clean::CleanArgs),
+    /// Get types of a given query
+    #[command(alias = "q")]
+    Query(query::QueryCommand),
+    /// Config
+    #[command(alias = "cfg")]
+    Config(config::ConfigCommand),
+    /// Alias for "schema diff"
+    #[command(alias = "d")]
+    Diff(schema::diff::DiffArgs),
+}
+
+pub async fn run(cfg: &Config) -> Result<()> {
+    let args = Cli::parse();
+    match &args.command {
+        Commands::Schema(cmd_args) => {
+            schema::cmd(&cfg, cmd_args).await?;
+        }
+        Commands::Clean(cmd_args) => {
+            clean::cmd(&cfg, cmd_args).await?;
+        }
+        Commands::Query(args) => query::cmd(&cfg, args).await?,
+        Commands::Config(args) => config::cmd(&cfg, args).await?,
+        Commands::Diff(args) => schema::diff::cmd(&cfg, args).await?,
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
 use anyhow::Result;
-use clap::{Parser, Subcommand, ValueEnum};
-use commands::{
-    clean::CleanArgs,
-    config::ConfigCommand,
-    query::QueryCommand,
-    schema::{diff::DiffArgs, SchemaCommand},
-};
+use clap::ValueEnum;
 use config::Config;
 
 pub mod commands;
 pub mod config;
-
-#[derive(Debug, Parser)]
-#[command(name = "tusker")]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    Schema(SchemaCommand),
-    /// Remove all temporary databases, schemas and tables created by
-    /// tusker
-    Clean(CleanArgs),
-    /// Get types of a given query
-    #[command(alias = "q")]
-    Query(QueryCommand),
-    /// Config
-    #[command(alias = "cfg")]
-    Config(ConfigCommand),
-    /// Alias for "schema diff"
-    #[command(alias = "d")]
-    Diff(DiffArgs),
-}
 
 #[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
 enum Backend {
@@ -54,19 +23,7 @@ impl std::fmt::Display for Backend {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Cli::parse();
     let cfg = Config::new()?;
-
-    match &args.command {
-        Commands::Schema(cmd_args) => {
-            commands::schema::cmd(&cfg, cmd_args).await?;
-        }
-        Commands::Clean(cmd_args) => {
-            commands::clean::cmd(&cfg, cmd_args).await?;
-        }
-        Commands::Query(args) => commands::query::cmd(&cfg, args).await?,
-        Commands::Config(args) => commands::config::cmd(&cfg, args).await?,
-        Commands::Diff(args) => commands::schema::diff::cmd(&cfg, args).await?,
-    }
+    commands::run(&cfg).await?;
     Ok(())
 }
